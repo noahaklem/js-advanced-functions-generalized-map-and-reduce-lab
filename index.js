67d941d29e7a77ca199bcfd8ca951d25106ea7b6
Noah A Klem <noahaklem@gmail.com>
2021-11-10 19:06:19 -0700

    commit tree f9979eec3178b778e179b6f9a027d26aebe36649

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+function map(array, fn) {
+    let results = [];
+
+    for (let i = 0; i < array.length; i++) {
+        results.push(fn(array[i]))
+    }
+    return results
+}
+
+function reduce(array, fn, start) {
+    let total;
+    if (start) {
+        total = start;
+        for (let i = 0; i < array.length; i++) {
+            total = fn(array[i], total)
+        }
+        return total
+    } else {
+        total = array[0];
+        for (let i = array[0]; i < array.length; i++) {
+            total = fn(array[i], total)
+        }
+        return total
+    }
+}
+//sourceArray = [1, 2, true, "razmatazz"] , function(a, memo){ return !!a && !!memo}
+// sourceArray = [1,2,3]
+//       startingPoint = 100
